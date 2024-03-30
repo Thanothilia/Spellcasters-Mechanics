80ad8cbcb28a5a1b5a7d5d822f2feab546e1a277
AshesAndFlames <163905732+AshesAndFlames@users.noreply.github.com>
2024-03-30 13:28:03 -0800

    Update Deadtest.cpp

--- Deadtest.cpp
+++ Deadtest.cpp
@@ -13,20 +13,19 @@ bool deadtest(float fire, float earth, float water, float air )
         if (dmg>health){return true;}
         else{return false;}
         break;
-
     case 1:
         // Fish (water type)
         dmg = (fire*2 + earth + water/2 + air)
         if (dmg>health){return true;}
         else{return false;}
         break;
-    case 1:
+    case 2:
         // Golem (earth type)
         dmg = (fire + earth/2 + water + air*2)
         if (dmg>health){return true;}
         else{return false;}
         break;
-    case 1:
+    case 3:
         // Whirlwind (wind type)
         dmg = (fire + earth*2 + water + air/2)
         if (dmg>health){return true;}
