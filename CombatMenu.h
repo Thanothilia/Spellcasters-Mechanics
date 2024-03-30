f2af12f0978ee996522196cc7b8725d31ed86d19
Thanothilia <49969493+Thanothilia@users.noreply.github.com>
2024-03-30 13:54:17 -0800

    Add files via upload

---
+++ CombatMenu.h
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void CombatMenu()
+{
+    int playerChoice;
+
+    do{
+    cout<<"1. Fight"<<endl;
+    cout<<"2. Flee"<<endl;
+
+    switch(playerChoice)
+    {
+        case 1:
+            //funtion for spell list
+            break;
+
+        case 2:
+            //roll random to escape
+    }
+    }
+    while(/*All enemies are dead*/);
+}
