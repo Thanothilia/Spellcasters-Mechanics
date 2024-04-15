2f204769a76ecb72e1b6d61f03f08b5166284ceb
Thanothilia <49969493+Thanothilia@users.noreply.github.com>
2024-04-14 18:18:55 -0800

    Update Spellcasters.cpp

--- Spellcasters.cpp
+++ Spellcasters.cpp
@@ -71,36 +71,54 @@ void liminal()
 
 
 
-bool deadtest(float fire, float earth, float water, float air )
-{
+void deadtest(float &fire, float &earth, float &water, float &air, int* enemies){
     float dmg, health;
     health = 2;
-    switch (enemylist[enemytype])  {
-    case 0:
+    if (enemies[0] > 0){
         // Imp (fire type)
         dmg = (fire/2 + earth + water * 2 + air);
-        if (dmg>=health){return true;}
-        else{return false;}
-        break;
-    case 1:
+        if (dmg>=health){
+            cout << "Imp has been defeated! \n";
+            enemies[0] = 0;
+        }
+        else{
+            cout << "Imp is still alive...\n";
+            }
+    }
+    if(enemies[1] > 0){
         // Fish (water type)
         dmg = (fire*2 + earth + water/2 + air);
-        if (dmg>=health){return true;}
-        else{return false;}
-        break;
-    case 2:
+        if (dmg>=health){
+            cout << "Fish has been defeated! \n";
+            enemies[1] = 0;
+        }
+        else{
+            cout << "Fish is still alive...\n";
+        }
+    }
+    if(enemies[2] > 0){
         // Golem (earth type)
         dmg = (fire + earth/2 + water + air*2);
-        if (dmg>=health){return true;}
-        else{return false;}
-        break;
-    case 3:
+        if (dmg>=health){
+            cout << "Golem has been defeated! \n";
+            enemies[2] = 0;
+            }
+        else{
+            cout << "Golem is still alive...\n";
+            }
+    }
+    if(enemies[3] > 0){
         // Whirlwind (wind type)
         dmg = (fire + earth*2 + water + air/2);
-        if (dmg>=health){return true;}
-        else{return false;}
-        break;
-}
+        if (dmg>=health){
+            cout << "Whirlwind has been defeated! \n";
+            enemies[3] = 0;
+            }
+        else{
+            cout << "Whirlwind is still alive...\n";
+            }
+        }
+
 }
 
 
